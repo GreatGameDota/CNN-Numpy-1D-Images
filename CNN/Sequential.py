0641f0196e37a9465d856a2b6fc91d7462810380
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-09-25 10:36:50 -0500

    Add Conv2D , MaxPool2D, and Flatten funcs

--- CNN/Sequential.py
+++ CNN/Sequential.py
@@ -2,13 +2,24 @@ from Conv2D import Conv2D
 from Dense import Dense
 from MaxPool2D import MaxPool2D
 
+
 class Sequential():
 
-  def __init__(self, layers=None):
-    self._layers = []
-    if layers:
-      for layer in layers:
-        self.add(layer)
+    def __init__(self, layers=None):
+        self._layers = []
+        if layers:
+            for layer in layers:
+                self.add(layer)
 
-  def add(self, layer):
-    self._layers.append(layer)
+    def add(self, layer):
+        self._layers.append(layer)
+        if (isinstance(layer, Conv2D)):
+            if (self._layers):
+                layer.setPrevFilters(
+                    self._layers[len(self._layers) - 1]._filters)
+            else:
+                layer.setPrevFilters(1)
+
+    def fit(self, X_Train, Y_train):
+        # TODO
+        X_Train /= 255
