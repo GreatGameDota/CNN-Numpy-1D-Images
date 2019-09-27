c31be5eaed94b4472ef798c6e8e054a7d93da7fe
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-09-27 10:03:30 -0500

    Update README

---
+++ README.md
@@ -0,0 +1,12 @@
+# ConvNet Library using Numpy
+
+Library inspired by Keras and TensorFlow  
+Main math functions from Alejandro Escontrela's [Numpy CNN](https://github.com/Alescontrela/Numpy-CNN)  
+
+---
+
+Example CNN made to classify the MNIST dataset  
+Trained on 100 images randomly shuffled 100 times, training only takes 40 minutes  
+After training model achieved 88% accuracy on test dataset  
+
+<img src='https://github.com/GreatGameDota/CNN-Numpy-Library/blob/master/github/accuracy.png?raw=true' alt='accuracy' title='accuracy'>
--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from CNN.Sequential import Sequential
 # Data prep
 import numpy as np
@@ -5,15 +6,26 @@ from keras.datasets import mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 y_train = np.reshape(y_train, (len(y_train), 1))
 x_train = np.reshape(x_train, (len(x_train), 28 * 28))
+y_test = np.reshape(y_test, (len(y_test), 1))
+x_test = np.reshape(x_test, (len(x_test), 28 * 28))
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
 
+# im = Image.open("sample.png")
+# sample = np.array(im.getdata())
+# sample = np.delete(sample, 1, 1)  # Delete R, G, B channels
+# sample = np.delete(sample, 1, 1)
+# sample = np.delete(sample, 1, 1)
+# sample = np.reshape(sample, (28, 28))
+# sample = sample.astype('float32')
+# sample /= 255
+
 model = Sequential()
 model.load_model('model.pkl')
-# pred, prob = model.predict_classes(x_test[0])
+# pred, prob = model.predict_classes(x_test[892])
 # print(pred)
-# print(y_test[0])
+# print(y_test[892])
 acc = model.evaluate(x_test, y_test)
-print(acc)
+# print(acc)
